6f74c6a3d87068491af84451bb8328e525f5b9f2
Abanoub-Abd-Elmessih <abanoubabdelmessih110@gmail.com>
2025-03-02 22:18:18 +0200

    add animated charts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,7 @@ export { Navbar } from "./navbar/Navbar";
 export { Footer } from "./footer/Footer";
 export { Menu } from "./menu/Menu";
 export { TopBox } from "./topBox/TopBox";
-export {ChartBox} from './chartBox/ChartBox'
+export { ChartBox } from "./chartBox/ChartBox";
+export { BarChartBox } from "./BarChartBox/BarChartBox";
+export { PieChartBox } from "./PieChartBox/PieChartBox";
+export { BigChartBox } from "./BigChartBox/BigChartBox";
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,43 @@
-import { ChartBox, TopBox } from "../../components";
-import { chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data";
+import { BarChartBox, BigChartBox, ChartBox, PieChartBox, TopBox } from "../../components";
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
 import "./home.scss";
 export default function Home() {
   return (
     <div className="home">
       <div className="box box1">
-        <TopBox/>
+        <TopBox />
       </div>
-      <div className="box box2"><ChartBox {...chartBoxUser}/></div>
-      <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
-      <div className="box box4"></div>
-      <div className="box box5"><ChartBox {...chartBoxConversion}/></div>
-      <div className="box box6"><ChartBox {...chartBoxRevenue}/></div>
-      <div className="box box7">BOX</div>
-      <div className="box box8">BOX</div>
-      <div className="box box9">BOX</div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box4">
+        <PieChartBox/>
+      </div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
+      <div className="box box7">
+        <BigChartBox/>
+      </div>
+      <div className="box box8">
+        <BarChartBox {...barChartBoxVisit} />
+      </div>
+      <div className="box box9">
+        <BarChartBox {...barChartBoxRevenue} />
+      </div>
     </div>
   );
 }
